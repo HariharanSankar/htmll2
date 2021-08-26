18ebfaafe47d1d6fe10034bb3a005cf3086b20eb
Hariharan Sankar <ha20182680@wipro.com>
2021-08-26 20:47:23 +0530

    Added Html l2

---
+++ script.js
@@ -0,0 +1,29 @@
+var x = document.getElementById("pos"); 
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported in this browser";
+    }
+}
+function showPosition(position) {
+    x.innerHTML = "Latitude : " + position.coords.latitude + "<br>Longitude : " + position.coords.longitude;
+}
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text"); ev.target.appendChild(document.getElementById(data));
+}
+
+if (typeof (Storage) !== "undefined") {
+    localStorage.location = localStorage.getItem(getLocation()).toString(); localStorage.setItem("location", localStorage.location); document.getElementById("result").innerHTML = localStorage.getItem("location");
+} else {
+    document.getElementById("result").innerHTML = "Your browser does not support Web Storage! Try another";
+}
